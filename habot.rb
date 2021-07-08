377b96949842ac9df42ef8f0aa86b486c818f84e
mznh <hiromtym@gmail.com>
2021-07-08 17:36:42 +0900

    replace tab to space

--- habot.rb
+++ habot.rb
@@ -5,11 +5,11 @@ require 'pp'
 
 class Markov 
   ORDER_NUM = 2
-	def initialize
+  def initialize
     
-		@word_list =[]
-		@chain_list =[]
-		@post
+    @word_list =[]
+    @chain_list =[]
+    @post
     File.open("wakati_tweet", "r") do |f|
       f.each_line do |line|
         tmp = line.chomp.split(" ")
@@ -21,23 +21,23 @@ class Markov
     end
 
     #pp @chain_list
-	end
+  end
 
-	def make_chain(pre_steps)
+  def make_chain(pre_steps)
     # そのうちn次のマルコフ連鎖にしたい
     near_list = @chain_list.select{|tuple| tuple[0]==pre_steps[0] }
-		if near_list.length == 0 then 
-			return pre_steps + [nil]
-		end
+    if near_list.length == 0 then 
+      return pre_steps + [nil]
+    end
     return near_list.sample
-	end
+  end
 
-	def generate_post
+  def generate_post
     post = ""
-		first_word_list=["今日","天地","タレ","ブタ","生","麺","汁","こりゃ","豚","醤油"]
+    first_word_list=["今日","天地","タレ","ブタ","生","麺","汁","こりゃ","豚","醤油"]
     now_word =  first_word_list.sample
     400.times do
-			now_word, next_word = make_chain([now_word])
+      now_word, next_word = make_chain([now_word])
       post += now_word
 
       if next_word.nil? or now_word.include? "完飲" then
@@ -46,9 +46,9 @@ class Markov
 
       # next step
       now_word = next_word
-		end
+    end
     post
-	end
+  end
 end
 
 
